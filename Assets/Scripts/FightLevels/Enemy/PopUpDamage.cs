4885308e2ab95704d21b2d0e8db9422e6bd9d006
txger. <simaocampanico.dev@gmail.com>
2025-03-24 12:02:54 +0000

    Merging PopUpDamage code

--- Assets/Scripts/FightLevels/Enemy/PopUpDamage.cs
+++ Assets/Scripts/FightLevels/Enemy/PopUpDamage.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 
 public class PopUpDamage : MonoBehaviour
 {
-    public Vector2 InitialVelocity;
-    public Rigidbody2D rb;
-    public float lifetime = 1.5f;
+    [SerializeField] private Vector2 InitialVelocity;
+    [SerializeField] private float lifetime = .5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb.linearVelocity = InitialVelocity;
+        GetComponent<Rigidbody2D>().linearVelocity = InitialVelocity;
         Destroy(gameObject, lifetime);
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
