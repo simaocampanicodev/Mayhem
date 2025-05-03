a048907f8f9c7cf3307fa99f9ad62d8a2ac9ee3e
Simão <simaocampanico.dev@gmail.com>
2025-05-03 15:41:57 +0100

    Removing every OkapiKit from the player and changing it movement to a new script. Horizontal flip is already available and he walks in the balcony with a regular speed

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private bool isFacingRight = false;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed * 7, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
