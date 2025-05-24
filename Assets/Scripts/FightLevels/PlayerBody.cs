1c376832e1b8145bdf2b27382809c81653e7dd1b
DinisB <dinisbarroso.dev@gmail.com>
2025-05-24 14:56:16 +0100

    Lose animation

--- Assets/Scripts/FightLevels/PlayerBody.cs
+++ Assets/Scripts/FightLevels/PlayerBody.cs
@@ -1,15 +1,18 @@
 using System.Collections;
-using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBody : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private GruntScript hurtSound;
     private SpriteRenderer spr;
+    [SerializeField] private bool PlayerBod;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (PlayerBod) { StartCoroutine(LoseTimer()); }
         spr = gameObject.GetComponent<SpriteRenderer>();
         AudioSource radioSource = gameObject.AddComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
@@ -27,12 +30,34 @@ public class PlayerBody : MonoBehaviour
 
     IEnumerator FadeBody()
     {
-        while (spr.color.a > 0f) {
+        while (spr.color.a > 0f)
+        {
             Color c = spr.color;
             c.a = Mathf.Max(0f, c.a - 0.1f);
             spr.color = c;
             yield return new WaitForSeconds(1f);
         }
         Destroy(gameObject);
+    }
+
+    IEnumerator LoseTimer()
+    {
+        Image fadeImage = GameObject.Find("Black").GetComponent<Image>();
+        yield return new WaitForSeconds(6f);
+        Color c = fadeImage.color;
+        float timer = 0f;
+
+        while (timer < 4)
+        {
+            timer += Time.deltaTime;
+            c.a = Mathf.Lerp(0f, 1f, timer / 4);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = 1f;
+        fadeImage.color = c;
+
+        SceneManager.LoadSceneAsync("TitleScreen");
     }
 }
