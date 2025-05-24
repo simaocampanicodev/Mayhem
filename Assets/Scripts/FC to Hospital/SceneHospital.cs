2d2d173bc22469dc2b8a48781a8c405aa1a88baf
DinisB <dinisbarroso.dev@gmail.com>
2025-05-24 15:22:54 +0100

    Fixed a game design related bug with the results

--- Assets/Scripts/FC to Hospital/SceneHospital.cs
+++ Assets/Scripts/FC to Hospital/SceneHospital.cs
@@ -8,14 +8,23 @@ public class SceneHospital : MonoBehaviour
     [SerializeField] private GameObject good2;
     [SerializeField] private GameObject bad1;
     [SerializeField] private GameObject bad2;
+    private float stress = 50;
+    private float stressFactor;
     void Start()
     {
         KeepGameData data = GameObject.Find("KeepFightData").GetComponent<KeepGameData>();
+        GameObject dataObj = GameObject.Find("KeepCoffeeData");
+        if (dataObj != null)
+        {
+            KeepGameData datatwo = dataObj.GetComponent<KeepGameData>();
+            stress = datatwo.stress;
+        }
+        stressFactor = 1f - (stress / 100f);
         int life = data.life;
-        if (life > 75) { Instantiate(good1); }
-        if (life <= 75 && life > 50) { Instantiate(good2); }
-        if (life <= 50 && life > 25) { Instantiate(bad1); }
-        if (life <= 25) { Instantiate(bad2); }
+        if (life > 75*stressFactor) { Instantiate(good1); }
+        if (life <= 75*stressFactor && life > 50*stressFactor) { Instantiate(good2); }
+        if (life <= 50*stressFactor && life > 25*stressFactor) { Instantiate(bad1); }
+        if (life <= 25*stressFactor) { Instantiate(bad2); }
         StartCoroutine(LoadSceneAfterDelay());
     }
 
