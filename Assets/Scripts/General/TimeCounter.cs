02280b07b429d42d77e8a602f626bc478149cb97
DinisB <dinisbarroso.dev@gmail.com>
2025-05-24 15:45:15 +0100

    Time counter

---
+++ Assets/Scripts/General/TimeCounter.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Image minuteDigit;
+    public Image secondTens;
+    public Image secondUnits;
+
+    public Sprite[] digitSprites;
+    public int startMinutes = 1;
+    public int startSeconds = 30;
+
+    private float remainingTime;
+
+    void Start()
+    {
+        remainingTime = startMinutes * 60 + startSeconds;
+    }
+
+    void Update()
+    {
+        if (remainingTime > 0f)
+        {
+            remainingTime -= Time.deltaTime;
+            if (remainingTime < 0f) remainingTime = 0f;
+        }
+
+        int totalSeconds = Mathf.FloorToInt(remainingTime);
+        int minutes = Mathf.Clamp(totalSeconds / 60, 0, 9);
+        int seconds = totalSeconds % 60;
+
+        int sTens = seconds / 10;
+        int sUnits = seconds % 10;
+
+        if (digitSprites.Length >= 10)
+        {
+            minuteDigit.sprite = digitSprites[minutes];
+            secondTens.sprite = digitSprites[sTens];
+            secondUnits.sprite = digitSprites[sUnits];
+        }
+    }
+}
