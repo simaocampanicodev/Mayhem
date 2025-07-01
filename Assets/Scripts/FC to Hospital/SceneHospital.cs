78ce2a98130049ef4b49b2a20a00ef3e857ce72e
DinisB <dinisbarroso.dev@gmail.com>
2025-07-01 10:58:59 +0100

    Added skip the hospital in case life was not lost

--- Assets/Scripts/FC to Hospital/SceneHospital.cs
+++ Assets/Scripts/FC to Hospital/SceneHospital.cs
@@ -11,6 +11,8 @@ public class SceneHospital : MonoBehaviour
     private float stress = 50;
     private float stressFactor;
     [SerializeField] private string sceneName;
+    [SerializeField] private string sceneSkipName;
+    private int life;
     void Start()
     {
         KeepGameData data = GameObject.Find("KeepFightData").GetComponent<KeepGameData>();
@@ -21,7 +23,7 @@ public class SceneHospital : MonoBehaviour
             stress = datatwo.stress;
         }
         stressFactor = 1f - (stress / 100f);
-        int life = data.life;
+        life = data.life;
         if (life > 75*stressFactor) { Instantiate(good1); }
         if (life <= 75*stressFactor && life > 50*stressFactor) { Instantiate(good2); }
         if (life <= 50*stressFactor && life > 25*stressFactor) { Instantiate(bad1); }
@@ -32,7 +34,7 @@ public class SceneHospital : MonoBehaviour
     private IEnumerator LoadSceneAfterDelay()
     {
         yield return new WaitForSeconds(6f);
-        
-        SceneManager.LoadScene(sceneName);
+        if (life == 100 * stressFactor) { SceneManager.LoadScene(sceneSkipName); }
+        else { SceneManager.LoadScene(sceneName); }
     }
 }
